e4d489ffdb5922d3aa88180d394d57c612667566
Marcin Gromek <mgromek@efigence.com>
2017-09-18 19:38:09 +0200

    add drawer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import Drawer from 'material-ui/Drawer';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
+
   render() {
     return (
       <div className="App">
         <AppBar
           title="Title"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap = {() => this.setState({open: !this.state.open})}
         />
+
+        <Drawer 
+          docked={false}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({ open })}
+        >
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
     );
   }
